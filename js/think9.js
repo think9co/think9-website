9347dd0dc5af40e96dc54f7977f08fc154e43f93
saurabhkt-think9 <saurabh@think9co.in>
2023-02-02 16:18:31 +0530

    Updated portfolio brands list and added brand website URLs to on click.

--- js/think9.js
+++ js/think9.js
@@ -38,15 +38,15 @@ $(function() {
 		}, 1000);
 	}
 
-	$('#navbarMain a').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-				var target = $(this.hash);
-				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-				if (target.length) {
-					scrollToSection(target);
-					return false;
-				} 
-		}
-	});
+	// $('#navbarMain a').click(function() {
+	// 	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	// 			var target = $(this.hash);
+	// 			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	// 			if (target.length) {
+	// 				scrollToSection(target);
+	// 				return false;
+	// 			} 
+	// 	}
+	// });
 
 });
